1366aaf19e7df4d3c55533cc59b1b274f619007b
Крис <kristinakulik.1993@gmail.com>
2023-04-11 04:00:55 +0400

    2

--- HW.sql
+++ HW.sql
@@ -1,38 +1,27 @@
 CREATE DATABASE skypro;
-CREATE DATABASE
 \c skypro
 
 CREATE TABLE employee (
-    skypro(# id BIGSERIAL NOT NULL PRIMARY KEY,
-        skypro(# first_name VARCHAR(50) NOT NULL,
-        skypro(# last_name VARCHAR(50) NOT NULL,
-        skypro(# gender VARCHAR(6) NOT NULL,
-        skypro(# age INT NOT NULL
-        skypro(# );
-CREATE TABLE
-    skypro=# INSERT INTO employee (
-                                      skypro(# first_name, last_name, gender, age)
-    skypro-# VALUES ('John', 'Depp', 'male', 59);
-    INSERT 0 1
-    skypro=# INSERT INTO employee (
-                                      skypro(# first_name, last_name, gender, age)
-    skypro-# VALUES ('Orlando', 'Bloom', 'male', 46);
-    INSERT 0 1
-    skypro=# INSERT INTO employee (
-                                      skypro(# first_name, last_name, gender, age)
-    skypro-# VALUES ('Keira', 'Knightley', 'female', 38);
-    INSERT 0 1
-    skypro=# SELECT * FROM employee;
-    skypro=# UPDATE employee SET first_name = 'Geoffrey' WHERE id = 3;
-    UPDATE 1
-    skypro=# UPDATE employee SET last_name = 'Rush' WHERE id = 3;
-    UPDATE 1
-    skypro=# UPDATE employee SET gender = 'male' WHERE id = 3;
-    UPDATE 1
-    skypro=# UPDATE employee SET age = 71 WHERE id = 3;
-    UPDATE 1
-    skypro=# SELECT * FROM employee;
-
-    skypro=# DELETE FROM employee WHERE id = 2;
-    DELETE 1
-    skypro=# SELECT * FROM employee;
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    gender VARCHAR(6) NOT NULL,
+    age INT NOT NULL
+   );
+    INSERT INTO employee (
+    first_name, last_name, gender, age)
+    VALUES ('John', 'Depp', 'male', 59);
+    INSERT INTO employee (
+    first_name, last_name, gender, age)
+    VALUES ('Orlando', 'Bloom', 'male', 46);
+    INSERT INTO employee (
+    first_name, last_name, gender, age)
+    VALUES ('Keira', 'Knightley', 'female', 38);
+    SELECT * FROM employee;
+    UPDATE employee SET first_name = 'Geoffrey' WHERE id = 3;
+    UPDATE employee SET last_name = 'Rush' WHERE id = 3;
+    UPDATE employee SET gender = 'male' WHERE id = 3;
+    UPDATE employee SET age = 71 WHERE id = 3;
+    SELECT * FROM employee;
+    DELETE FROM employee WHERE id = 2;
+    SELECT * FROM employee;
